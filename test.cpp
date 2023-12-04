83be316c691fbbc7470694b5e31eccc16edba4e7
hmjmf <zqx5449@gmail.com>
2023-12-04 08:20:19 +0000

    bazel support

---
+++ test.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <iostream>
+// #include "phxpaxos/sm.h"
+
+// class PhxEchoServer
+// {
+// public:
+//     PhxEchoServer();
+//     ~PhxEchoServer();
+
+//     int Echo(const std::string & sEchoReqValue, std::string & sEchoRespValue);
+// };
+
+// class PhxEchoSM : public phxpaxos::StateMachine
+// {
+// public:
+//     PhxEchoSM();
+
+//     bool Execute(const int iGroupIdx, const uint64_t llInstanceID, 
+//             const std::string & sPaxosValue, phxpaxos::SMCtx * poSMCtx);
+
+//     const int SMID() const { return 1; }
+// };
+
+int main() {
+    // PhxEchoSM p();
+    return 0;
+}
